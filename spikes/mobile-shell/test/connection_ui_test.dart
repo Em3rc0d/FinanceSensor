bf1174ce84b23dcaeefa5d6ee1cd5a23c6ec9f94
Eduardo Merino <123134857+Em3rc0d@users.noreply.github.com>
2026-09-02 17:42:12 -0500

    fix: import Material types in Gmail connection tests

--- spikes/mobile-shell/test/connection_ui_test.dart
+++ spikes/mobile-shell/test/connection_ui_test.dart
@@ -1,4 +1,5 @@
 import 'package:financesensor_mobile_shell/main_connected.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
